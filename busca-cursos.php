98445ab521cd0db2106d4097e3e7127a6e9b260f
Julia <juliapereiraengc@gmail.com>
2022-08-03 18:17:01 -0300

    Projeto buscador de cursos do alura feito no curso de composer

---
+++ busca-cursos.php
@@ -0,0 +1,16 @@
+<?php
+
+use GuzzleHttp\Client;
+use OliveiraJulia\AulaComposer\Buscador;
+use Symfony\Component\DomCrawler\Crawler;
+
+require "vendor\autoload.php";
+
+
+$client = new Client(['base_uri' => 'https://www.alura.com.br/', "verify"=> false]);
+$crawler = new Crawler();
+
+$buscardor = new Buscador($client, $crawler);
+$cursos = $buscardor->buscar('/cursos-online-programacao/php');
+
+foreach ($cursos as $curso) echo exibeMensagem($curso);
